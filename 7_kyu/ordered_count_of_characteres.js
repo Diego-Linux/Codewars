d540df2fdbd07c588217e9646a16b5a080d24515
Diego Oliveira <diegosudolinux@gmail.com>
2022-09-21 20:12:01 -0300

    Ordered Count of Characteres.js

---
+++ 7_kyu/ordered_count_of_characteres.js
@@ -0,0 +1,13 @@
+// Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+
+// Consult the solution set-up for the exact data structure implementation depending on your language.
+
+// Example:
+
+// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+// Link: https://www.codewars.com/kata/57a6633153ba33189e000074
+
+// The intent of the solution below is to be short, don't consider good practices
+
+const orderedCount = (txt) => [...new Set(txt)].map(x => [x, txt.split(x).length - 1])
