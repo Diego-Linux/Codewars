973a1b87a1e4a01e67a70256f3539b5e18eb8d89
Diego Oliveira <diegosudolinux@gmail.com>
2022-09-22 22:09:21 -0300

    Vowel count

---
+++ 7_kyu/vowel_count.js
@@ -0,0 +1,12 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// Link: https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+
+
+getCount = str => str.match(/[aeiou]/gi).length
+
+//   console.log(getCount('abracadabra'))
