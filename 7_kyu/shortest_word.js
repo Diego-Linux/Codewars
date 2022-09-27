f1bc381c279f658d1cfcd47aeb93c255eb1e5e11
Diego Oliveira <diegosudolinux@gmail.com>
2022-09-27 20:08:58 -0300

    Shortest word

---
+++ 7_kyu/shortest_word.js
@@ -0,0 +1,8 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// Link: https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
+
+
+findShort = (s) => s.split(" ").sort((a, b) => a.length - b.length)[0].length;
